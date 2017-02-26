16a72de44e124493c481f232694b01f24d8d27f5
Hank <loveinbox.hank@gmail.com>
2017-02-26 10:12:01 +0800

    add http interceptors

--- build/main.js
+++ build/main.js
@@ -4,6 +4,26 @@ angular.module('erp.directives', [])
 angular.module('erp.services', ['ngResource'])
 
 angular.module('erp')
+  .config(function($httpProvider) {
+    $httpProvider.interceptors.push(function($q) {
+      return {
+        'response': function(response) {
+          if (response.data.code === undefined) {
+            return response;
+          } else {
+            if (response.data.code === 0) {
+              return response.data;
+            } else {
+              alet(response.msg);
+              return $q.reject(response);
+            }
+          }
+        }
+      };
+    });
+  });
+
+angular.module('erp')
   .config(function($stateProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
